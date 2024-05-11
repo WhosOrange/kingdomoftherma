c4f42f00c626e376c2dd788ab2275319a9cef60f
Orange Derek <158000262+WhosOrange@users.noreply.github.com>
2024-05-10 22:18:52 -0700

    Backed up graded version of Kingdom of Therma's universal script

--- kot_uni.js
+++ kot_uni.js
@@ -34,3 +34,41 @@ function navBarHover(input) {
 };
 
 navBarHover(10);
+
+/////////////////////////////////
+// Navigation Bar
+var mapDisplay = document.getElementById("mapDisplay");
+
+function mapHover(input) {
+	switch (input) {
+		case 0:
+			mapDisplay.innerHTML = "Kingdom of Therma: Castle and Township";
+			break;
+		case 1:
+			mapDisplay.innerHTML = "Therma Woods Abandonend Temple";
+			break;
+		case 2:
+			mapDisplay.innerHTML = "Therma River";
+			break;
+		case 3:
+			mapDisplay.innerHTML = "Volcanic Fields";
+			break;
+		case 4:
+			mapDisplay.innerHTML = "Maze Lake";
+			break;
+		case 5:
+			mapDisplay.innerHTML = "Maze HQ";
+			break;
+		case 6:
+			mapDisplay.innerHTML = "Therma Sea";
+			break;
+		case 7:
+			mapDisplay.innerHTML = "Orph Territory";
+			break;
+		default:
+			mapDisplay.innerHTML = "<i>Hover Somewhere On The Map</i>";
+			break;
+	}
+};
+
+mapHover(10);
